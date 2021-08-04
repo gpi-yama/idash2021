fa2d79e8748273d67430b9e25a4c287e0e40b20f
Ryohei Yamaguchi <yama.pokerface1129@gmail.com>
2021-08-04 10:12:05 +0000

    [ADD] coef version server

--- src/server/ServerSide.h
+++ src/server/ServerSide.h
@@ -1,5 +1,6 @@
 #include "../common.h"
 #include "../ml/LogisticRegression.h"
+#include "../ml/CoefLogisticRegression.h"
 // #include "../ml/ML.h"
 
 namespace capsuleGene
@@ -8,9 +9,12 @@ namespace capsuleGene
     {
     private:
         LogisticRegression ml;
+        CoefLogisticRegression ml_coef;
+        bool use_coef = false;
 
     public:
         ServerSide(const LogisticRegression &ml);
+        ServerSide(const CoefLogisticRegression &ml);
         std::vector<std::vector<Ciphertext>> process(const std::vector<Ciphertext> &x);
         void load_weight(const std::string file_path);
         void load_bias(const std::string file_path);
