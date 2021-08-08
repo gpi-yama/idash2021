405b4b12978bf8b51d79f5f230dc1d5c60fb6d6d
Ryohei Yamaguchi <yama.pokerface1129@gmail.com>
2021-08-08 14:40:54 +0000

    [MOD] load params as npy

--- src/server/ServerSide.cpp
+++ src/server/ServerSide.cpp
@@ -16,19 +16,21 @@ namespace capsuleGene
 
     std::vector<std::vector<Ciphertext>> ServerSide::process(const std::vector<Ciphertext> &x)
     {
-        if (this->use_coef) return this->ml_coef.predict(x);
+        // if (this->use_coef) return this->ml_coef.predict(x);
         return this->ml.predict(x);
     }
 
     void ServerSide::load_weight(const std::string file_path)
     {
-        std::vector<std::vector<double>> weight = IOUtils::read_csv(file_path, false, false);
-        this->ml.set_weight(IOUtils::read_csv(file_path, false, false));
+        std::vector<float> weight;
+        IOUtils::read_vector_npy(file_path, weight);
+        this->ml.set_weight(weight);
     }
 
     void ServerSide::load_bias(const std::string file_path)
     {
-        std::vector<std::vector<double>> csv = IOUtils::read_csv(file_path, false, false);
-        this->ml.set_bias(csv);
+        std::vector<float> bias;
+        IOUtils::read_vector_npy(file_path, bias);
+        this->ml.set_bias(bias);
     }
 }
